34d22006ca7098a559bf6bb8d86d33db536f839e
Aljenan Ali <38101495+jenansol@users.noreply.github.com>
2024-07-23 08:15:44 +0300

    ..

--- login/views.py
+++ login/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from rest_framework.response import Response
 
 # Create your views here.
 from rest_framework import generics
