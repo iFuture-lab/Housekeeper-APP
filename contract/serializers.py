4373e7909c171fd8b4a642842ceb61cdf123eba3
Aljenan Ali <38101495+jenansol@users.noreply.github.com>
2024-08-27 14:42:30 +0300

    .

--- contract/serializers.py
+++ contract/serializers.py
@@ -7,11 +7,11 @@ from .models import UserInterest
 class UserInterestSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserInterest
-        fields = '__all__' 
-        
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation.pop('deleted_at', None)
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return UserInterest.objects.create(**validated_data)
+  
         
 
 class ContractSerializer(serializers.ModelSerializer):
